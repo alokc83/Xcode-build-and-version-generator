9805efc8880e488d6f386502a81124e80cb56a10
Alok Choudhary <alok.c83@gmail.com>
2015-05-01 09:51:50 -0400

    Working version from project
    scripts working version from project Chanakya

--- xcode-build-number-generator.sh
+++ xcode-build-number-generator.sh
@@ -9,56 +9,101 @@
 # 5. Drag the "Run Script" below "Link Binaries With Libraries"
 
 
-#Credits 
+#Credits
 # sekati@github for intial direction about automatic versioning
 # http://www.codinghorror.com/blog/2007/02/whats-in-a-version-number-anyway.html (For unferstanding the Software Versoining)
 #Feel free to leave comment or report issues
 
+#CHECKING WHICH info.plist need to be used
+if [[ -e InfoT2.plist ]]
+then
+plistFile="InfoT2.plist"
+else
+plistFile="${INFOPLIST_FILE}"
+fi
 
 MONTH=`date | awk '{print $2}'`
 
 case "$MONTH" in
-  	'Jan' | [Jj][Aa][Na] )
-         MONTHNUMBER=1
-	 ;;
-	'Feb' |[Ff][Ee][Bb])
-         MONTHNUMBER=2
-	;;
-	'Mar' |[Mm][Aa][Rr])
-	MONTHNUMBER=3
-	echo "Month is $MONTHNUMBER"
-	;;
-	'Apr' |[Aa][Pp][Rr])
-         MONTHNUMBER=4
-	;;
-	'May' |[Mm][Aa][Yy])
-         MONTHNUMBER=5
-        ;;
-	'Jun' |[Jj][Uu][Nn])
-         MONTHNUMBER=6
-        ;;
-	'Jul' |[Jj][Uu][Ll])
-         MONTHNUMBER=7
-        ;;
-	'Aug' |[Aa][Uu][Gg])
-         MONTHNUMBER=8
-        ;;
-	'Sep' |[Ss][Ee][Pp])
-         MONTHNUMBER=9
-        ;;
-	'Oct' |[Oo][Cc][Tt])
-         MONTHNUMBER=10
-        ;;
-	'Nov' |[Nn][Oo][Vv])
-         MONTHNUMBER=11
-        ;;
-	'Dec' |[Dd][Ee][Cc])
-         MONTHNUMBER=12
-        ;;
+'Jan' | [Jj][Aa][Na] )
+MONTHNUMBER="01"
+;;
+'Feb' |[Ff][Ee][Bb])
+MONTHNUMBER="02"
+;;
+'Mar' |[Mm][Aa][Rr])
+MONTHNUMBER="03"
+;;
+'Apr' |[Aa][Pp][Rr])
+MONTHNUMBER="04"
+;;
+'May' |[Mm][Aa][Yy])
+MONTHNUMBER="05"
+;;
+'Jun' |[Jj][Uu][Nn])
+MONTHNUMBER="06"
+;;
+'Jul' |[Jj][Uu][Ll])
+MONTHNUMBER="07"
+;;
+'Aug' |[Aa][Uu][Gg])
+MONTHNUMBER="08"
+;;
+'Sep' |[Ss][Ee][Pp])
+MONTHNUMBER="09"
+;;
+'Oct' |[Oo][Cc][Tt])
+MONTHNUMBER="10"
+;;
+'Nov' |[Nn][Oo][Vv])
+MONTHNUMBER="11"
+;;
+'Dec' |[Dd][Ee][Cc])
+MONTHNUMBER="12"
+;;
 esac
 
 DATE=`date | awk '{print $3}'`
 echo "Date = $DATE"
+
+case $DATE in
+1 )
+DATE="01"
+;;
+
+2 )
+DATE="02"
+;;
+
+3 )
+DATE="03"
+;;
+
+4 )
+DATE="04"
+;;
+
+5)
+DATE="05"
+;;
+
+6 )
+DATE="06"
+;;
+
+7 )
+DATE="07"
+;;
+
+8 )
+DATE="08"
+;;
+
+9 )
+DATE="09"
+;;
+esac
+
 YEAR=`date | awk '{print $6}'`
 echo "Date = $YEAR"
 
@@ -72,10 +117,12 @@ echo "Date = $YEAR"
 #### Uncomment only one one style or last one will be in effect
 #buildNumber=$MONTH$DATE$YEAR
 #buildNumber=$DATE$MONTH$YEAR
-buildNumber=$MONTHNUMBER$DATE$YEAR
+#buildNumber=$MONTHNUMBER$DATE$YEAR
 #buildNumber=$DATE$MONTHNUMBER$YEAR
+buildNumber=$YEAR$MONTHNUMBER$DATE
 
 
 echo "Final Build number is $buildNumber"
+echo "$plistFile"
 ## Below command write buildNumber in the property list
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${PROJECT_DIR}/${INFOPLIST_FILE}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$plistFile"
--- xcode-versionString-generator.sh
+++ xcode-versionString-generator.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # xcode-version-generator.sh
 # @desc Auto-increment the version number (only) when a project is archived for export.
 # @usage
@@ -23,186 +24,249 @@
 ## ONLY UNCOMMENT BELOW LINE FOR THE PURPOSE OF DEBUGGING
 #set -x
 
-######### DEBUG MSG function YES to Enable, No to Disable 
-DEBUG="NO" # Set DEBUG YES(Enable) or NO(Disable)
+######### DEBUG MSG function YES to Enable, No to Disable
+DEBUG="YES" # Set DEBUG YES(Enable) or NO(Disable)
+
+######### SYMBOLIC CONSTANT
+blackFlag="⚑"
+whiteFlag="⚐"
+leftBlackArrow="◀"
+rightBlackArrow="▶"
+doubleArrow="➤➤"
+crossmark1="✖"
+crossmark2="✕"
+crossmark3="✗"
+crossmark4="✘"
+tickmark1="✓"
+tickmark2="✔"
+
+star1="☆"
+star2="✩"
+star3="✪"
+star4="✭"
+
+info="ℹ"
+
+############# EXIT CODES
+notEnoughArgs=44
+
+### Echoing the name of script running [Debugging purpose]
+echo "$0 IS RUNNING."
 
 #DEFINEING THE CONST VALUES
-#plistFile="${PROJECT_DIR}/${INFOPLIST_FILE}"
+if [[ -e ${INFOPLIST_FILE} ]]
+then
+plistFile="${INFOPLIST_FILE}"
+else
 plistFile="InfoT2.plist"
+fi
 
-
-
-### DEBUG Message function 
-debugMsg()
+## Print info Msg and icon
+printInfo()
 {
-	if [[ $DEBUG == "YES" ]]
-	then
-	echo "DEBUG MESSAGE---> $1"
-	fi
+echo "($info) Info : $1"
 }
 
-#addPropertyToList()
-#{
-	#if [[ $# -ne 3 ]]
-	#	echo "Not enough arguments to add property to the list"
-		#exit 1
-	#else
-	#	echo " DO something "
-#	fi 
-	
-#}
+## Print Error msg and icon
+printError()
+{
+echo "($crossmark1) ERROR : $1"
+}
+
+printFlag()
+{
+echo "($blackFlag) : $1"
+}
 
 
-##### FUNCTION TO UPDATE plist 
+### DEBUG Message function
+debugMsg()
+{
+if [[ $DEBUG == "YES" ]]
+then
+echo "DEBUG MESSAGE---> $1"
+fi
+}
+
+addPropertyToList()
+{
+echo "Using provided arguments to create property."
+if [[ $# -ne 3 ]]
+then
+debugMsg "Number of argumets passed = $#"
+debugMsg "Arguments passed to function are #1=$1, #2=$2, #3=$3"
+printError "Not enough arguments to add property to the list"
+printFlag "Exiting script with Error Code $notEnoughArgs."
+printInfo "Find all exit codes in 'Exit Codes' section."
+exit $notEnoughArgs
+else
+echo "Creating property CustomPreviousBundleShortVersionString."
+/usr/libexec/PlistBuddy -c "Add :$1 $2 $3" "$plistFile"
+RC=$?
+if [[ RC -ne 0 ]]
+then
+echo "ERROR : $blackFlag $doubleArrow Exit with error code $?"
+exit $?
+else
+debugMsg "Property $1 Added to the $plistFile"
+fi
+fi
+debugMsg "end of Add Property function."
+}
+
+
+##### FUNCTION TO UPDATE plist
 updatePlist()
 {
-
+debugMsg "updatePlist fucntion starts."
 #/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $NEWVERSIONSTRING" "${PROJECT_DIR}/${INFOPLIST_FILE}"
 NEWVERSIONSTRING=`echo $MAJORVERSION.$MINORVERSION.$REVISION`
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $NEWVERSIONSTRING" $plistFile
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $NEWVERSIONSTRING" "$plistFile"
 debugMsg "Exit Code is $?"
 echo "Setting CFBundleShortVersionString property to $NEWVERSIONSTRING."
 
 #Setting up Previous Version String
 PVERSIONSTRING=`echo $PMAJORVERSION.$PMINORVERSION.$(($REVISION - 1))`
-/usr/libexec/PlistBuddy -c "Set :CustomPreviousBundleShortVersionString $PVERSIONSTRING" $plistFile
+/usr/libexec/PlistBuddy -c "Set :CustomPreviousBundleShortVersionString $PVERSIONSTRING" "$plistFile"
 debugMsg "Exit Code is $?"
 echo "Setting CustomPreviousBundleShortVersionString property to $PVERSIONSTRING."
 
+debugMsg "updatePlist function ends."
 }  #end of function
 
 
 versionValues()
 {
-	echo "Version values as follows:"
-	echo "Current version $VERSIONSTRING"
-	echo "New version NEWVERSIONSTRING"
-	echo "Previous version $PVERSIONSTRING"
+echo "Version values as follows:"
+echo "Current version $VERSIONSTRING"
+echo "New version NEWVERSIONSTRING"
+echo "Previous version $PVERSIONSTRING"
 } #End of funtion
 
 whatsUpdate()
 {
-	echo "$plist File is updated with follwoing values "
-	echo "New version: CFBundleShortVersionString = $NEWSUBVERSION"
-	echo "Previous version: CustomPreviousBundleShortVersionString = $PVERSIONSTRING"
+echo "$plist File is updated with follwoing values "
+echo "New version: CFBundleShortVersionString = $NEWSUBVERSION"
+echo "Previous version: CustomPreviousBundleShortVersionString = $PVERSIONSTRING"
 } #ENd of function
 
 #Extract Major, Minor and Revision from Version String
-extractVersionNumbers() 
+extractVersionNumbers()
 {
-	MAJORVERSION=`echo $VERSIONSTRING | awk -F "." '{print $1}'`
-	MINORVERSION=`echo $VERSIONSTRING | awk -F "." '{print $2}'`
-	REVISION=`echo $VERSIONSTRING | awk -F "." '{print $3}'`
+MAJORVERSION=`echo $VERSIONSTRING | awk -F "." '{print $1}'`
+MINORVERSION=`echo $VERSIONSTRING | awk -F "." '{print $2}'`
+REVISION=`echo $VERSIONSTRING | awk -F "." '{print $3}'`
 }
 
 #Extract Major, Minor and Revision from PRevious Version String
 extractPreviousVersionNumbers()
 {
-	#Extracting Previous Version 
-	PMAJORVERSION=`echo $PVERSIONSTRING | awk -F "." '{print $1}'`
-	PMINORVERSION=`echo $PVERSIONSTRING | awk -F "." '{print $2}'`
-	PREVISION=`echo $PVERSIONSTRING | awk -F "." '{print $3}'`
+#Extracting Previous Version
+PMAJORVERSION=`echo $PVERSIONSTRING | awk -F "." '{print $1}'`
+PMINORVERSION=`echo $PVERSIONSTRING | awk -F "." '{print $2}'`
+PREVISION=`echo $PVERSIONSTRING | awk -F "." '{print $3}'`
 }
 
 
+### DEBUGGING BLOCK, BEFORE DOING ANYTHING SERIOUS :)
+debugMsg "$plistFile"
+#DOLS=`ls "plistFile"`
+#DOLSRC=$?
+#debug "Result of ls on $plistFile is $DOLS with return code $DOLSRC"
 
-#Read the if Property file 
-# This splits a two-decimal version string, such as "0.45.123", allowing us to increment the third position.
-#VERSIONSTRING=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${PROJECT_DIR}/${INFOPLIST_FILE}")
-#PVERSIONSTRING=$(/usr/libexec/PlistBuddy -c "Print CustomPreviousBundleShortVersionString" "${PROJECT_DIR}/${INFOPLIST_FILE}")
+#### SOME SYSTEM VARS Details [Debuggin Purpose]
+PROJDIR="${PROJECT_DIR}"
+INFOPLIST="/${INFOPLIST_FILE}"
 
-#VERSIONBUILB=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
-#PVERSIONBUILD=$(/usr/libexec/PlistBuddy -c "Print CustomPreviousBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
+debugMsg "My arg = $plistFile";
+debugMsg "Actual Proj Dir = $PROJDIR"
+debugMsg "Actual info-plist location = $INFOPLIST"
 
-VERSIONSTRING=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $plistFile)
-VERSIONBUILB=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" $plistFile)
+#### DEBUG BLOCK ENDS ##########
 
-PVERSIONSTRING=$(/usr/libexec/PlistBuddy -c "Print CustomPreviousBundleShortVersionString" $plistFile)
+VERSIONSTRING=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$plistFile")
+PVERSIONSTRING=$(/usr/libexec/PlistBuddy -c "Print CustomPreviousBundleShortVersionString" "$plistFile")
 RETURNPVERSIONSTRING=$?
 debugMsg "Return code for version built $RETURNPVERSIONSTRING"
-PVERSIONBUILD=$(/usr/libexec/PlistBuddy -c "Print CustomPreviousBundleVersion" $plistFile)
+debugMsg "$PVERSIONSTRING"
+read myname
+VERSIONBUILD=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$plistFile")
+PVERSIONBUILD=$(/usr/libexec/PlistBuddy -c "Print CustomPreviousBundleVersion" "$plistFile")
 RETURNPVERSIONBUID=$?
-debugMsg "Return code for build number $RETURNPVERSIONBUID"
+debugMsg "Return code for previous build $RETURNPVERSIONBUID"
 
-#PVERSIONSTRING
-#VERSIONSTRING="1.2.0"
-#VERSIONBUILB='3212013'
+debugMsg "Version_string = $VERSIONSTRING, Previous Version=$PVERSIONSTRING"
+debugMsg "Version Build = $VERSIONBUILD, Previous Build = $PVERSIONBUILD"
+
 
 if [[ $RETURNPVERSIONSTRING -ne 0 ]] #Previous version property does not exists
 then
-	echo "Previous version string does not exists"
-	echo "Creating property CustomPreviousBundleShortVersionString."
-	#### AUTOMATIC PROPERTY CREATETOR CODE 
-	/usr/libexec/PlistBuddy -c "Add :CustomPreviousBundleShortVersionString String $VERSIONSTRING" $plistFile
-	echo "Write process is done, return code is $?. Zero means property created with value $VERSIONSTRING"
-	PVERSIONSTRING=$VERSIONSTRING
-else 
-	debugMsg "Previous version number exists, it is $PVERSIONSTRING."
-	extractVersionNumbers #Extract Major, Minor and Revision from Version String
-	extractPreviousVersionNumbers #Extract Major, Minor and Revision from PRevious Version String
-	
-	if [[ $MINORVERSION -gt $PMINORVERSION ]]
-	then
-		debugMsg "Minor version is incremented. Setting revision to Zero"
-		REVISION=0
-		updatePlist #Function Call
-		#versionValues #Function Call
-	else
-		PVERSIONSTRING=$VERSIONSTRING
-		debugMsg "Incrementing Revision By One"
-		REVISION=$(($REVISION + 1))
-		updatePlist #Function Call
-		#versionValues #Function Call
-		debugMsg "new = $NEWVERSIONSTRING, Previous = $PVERSIONSTRING"
-	fi
-	
+echo "Previous version string does not exists"
+#### AUTOMATIC PROPERTY CREATETOR CODE
+debugMsg "ARGUMENT TO BE PASSED $VERSIONSTRING, $plistFile"
+addPropertyToList "CustomPreviousBundleShortVersionString" "String" "$VERSIONSTRING"
+debugMsg "Write process is done, return code is $?. Zero means property created with value $VERSIONSTRING"
+PVERSIONSTRING=$VERSIONSTRING
+else
+debugMsg "Previous version number exists, it is $PVERSIONSTRING."
+extractVersionNumbers #Extract Major, Minor and Revision from Version String
+extractPreviousVersionNumbers #Extract Major, Minor and Revision from PRevious Version String
+
+if [[ $MINORVERSION -gt $PMINORVERSION ]]
+then
+debugMsg "Minor version is incremented. Setting revision to Zero"
+REVISION=0
+updatePlist #Function Call
+#versionValues #Function Call
+else
+PVERSIONSTRING=$VERSIONSTRING
+debugMsg "Incrementing Revision By One"
+REVISION=$(($REVISION + 1))
+updatePlist #Function Call
+#versionValues #Function Call
+debugMsg "new = $NEWVERSIONSTRING, Previous = $PVERSIONSTRING"
+fi
+
 fi
 
 #### SYNCING Current and Previous version String
 if [[ $MAJORVERSION -ne $PMAJORVERSION || $MINORVERSION -ne $PMINORVERSION ]]
-then 
-	echo "Current and previous version strings are out of sync. Manual override needed"
+then
+echo "Current and previous version strings are out of sync. Manual override needed"
 else
-	echo "Current and previous version string are in sync. No action needed"
+echo "Current and previous version string are in sync. No action needed"
 fi
 
 if [[ $REVISION -gt 0 && $REVISION < $PREVISION ]]
 then
-	PREVISION=$(($REVISION - 1))
-fi 
-
-if [[ $RETURNPVERSIONBUID -ne 0 ]]
-then    
-        echo "Previous Build Number does not exists"
-        echo "You need to make property CustomPreviousBundleVersion maually"
-	#### AUTOMATIC PROPERTY CREATETOR CODE IS BELOW
-	/usr/libexec/PlistBuddy -c "Add :CustomPreviousBundleVersion String $VERSIONBUILB" $plistFile
-	debugMSG "Write process is done, return code is $?"
-	PVERSIONBUILD=$VERSIONBUILB
-else
-        debugMsg "Previous build number exists, it is $PVERSIONBUILD."
+PREVISION=$(($REVISION - 1))
 fi
 
-debugMsg "[$PVERSIONSTRING]"
-debugMsg "[$PVERSIONBUILD]"
+######### CHECK IF PREVIOUS BUID EXISTS OR NOT #############
 
-#/usr/libexec/PlistBuddy -c "Add :CustomPreviousBundleShortVersionString String $VERSIONSTRING" "InfoT2.plist"
-#echo "Write process is done, return code is $?"
+if [[ $RETURNPVERSIONBUID -ne 0 ]]
+then
+debugMsg "Previous Build Number does not exists"
+#### AUTOMATIC PROPERTY CREATETOR CODE IS BELOW
+addPropertyToList "CustomPreviousBundleVersion" "String" "$VERSIONBUILD"
+debugMsg "Write process is done, return code is $?"
+PVERSIONBUILD=$VERSIONBUILB
+else
+debugMsg "Previous build number exists, it is $PVERSIONBUILD. current build = $VERSIONBUILD"
+if [[ $VERSIONBUILD -gt $PVERSIONBUILD ]]
+then
+PVERSIONBUILD=$VERSIONBUILD
+debugMsg "VERSIONBUILD is assinged to PVERSIONBUILD"
+/usr/libexec/PlistBuddy -c "Set :CustomPreviousBundleVersion $PVERSIONBUILD" "$plistFile"
+
+else
+debugMsg "VERSION BUILD AND PVERSIONBUILD ARE SAME"
+fi
+fi
+##############################################
+debugMsg "previous verison [$PVERSIONSTRING]"
+debugMsg "previous [$PVERSIONBUILD]"
+debugMsg "previous [$VERSIONBUILD]"
+
 
 ##########################################################################
-
-
-#NEWVERSIONSTRING=`echo $VERSIONSTRING | awk -F "." '{print $1 "." $2 ".'$NEWSUBVERSION'" }'`
-
-
-
-
-#/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $NEWVERSIONSTRING" "${PROJECT_DIR}/${INFOPLIST_FILE}"
-#/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $NEWVERSIONSTRING" "InfoT2.plist"
-#echo "Setting CFBundleShortVersionString property to $NEWVERSIONSTRING. Exit Code is $?"
-
-#Setting up Previous Version String
-#PVERSIONSTRING=`echo $PMAJORVERSION.$PMINORVERSION.$PREVISION`
-#/usr/libexec/PlistBuddy -c "Set :CustomPreviousBundleShortVersionString $PVERSIONSTRING" "InfoT2.plist"
-#echo "Setting CustomPreviousBundleShortVersionString property to $VERSIONSTRING. Exit Code is $?"
-
